5e5b10b4e99d1019956f1e92a135846b33366020
Axell <axelleagle@gmail.com>
2023-09-04 19:37:20 +0930

    add new dashboard html

---
+++ WCP Product Research Tool/script/dashboard.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+  $("#teamAmount").text(productChosen);
+});
--- WCP Product Research Tool/script/script.js
+++ WCP Product Research Tool/script/script.js
@@ -1,4 +1,7 @@
+var productChosen = "default";
+
 $(document).ready(function () {
+  var productChosen = "Test product1";
   var tabIdSelected = "tab0";
   var menuToggle = true;
 
@@ -22,95 +25,73 @@ $(document).ready(function () {
 });
 
 function contentEnable() {
-  document.getElementById("darkLayer").style.display = "none";
+  $("#darkLayer").hide();
 }
 function contentDisable() {
-  document.getElementById("darkLayer").style.display = "";
+  $("#darkLayer").show();
 }
 
 function menuExtend() {
-  const tabs = document.querySelectorAll(".tab-layout");
-  const tabNames = document.querySelectorAll(".tab-name");
-  const sidebar = document.getElementById("sidebar");
-  sidebar.classList.add("side-extended");
-  tabs.forEach((tab) => {
-    tab.classList.add("tab-layout-extended");
-  });
-  tabNames.forEach((tabName) => {
-    tabName.classList.add("tab-name-extended");
-  });
+  $("#sidebar").addClass("side-extended");
+  $(".tab-layout").addClass("tab-layout-extended");
+  $(".tab-name").addClass("tab-name-extended");
 }
 
 function menuCollapse() {
-  const tabs = document.querySelectorAll(".tab-layout");
-  const tabNames = document.querySelectorAll(".tab-name");
-  const sidebar = document.getElementById("sidebar");
-  tabs.forEach((tab) => {
-    tab.classList.remove("tab-layout-extended");
-  });
-  tabNames.forEach((tabName) => {
-    tabName.classList.remove("tab-name-extended");
-  });
-  sidebar.classList.remove("side-extended");
+  $(".tab-layout").removeClass("tab-layout-extended");
+  $(".tab-name").removeClass("tab-name-extended");
+  $("#sidebar").removeClass("side-extended");
 }
 
 function selectTab(tabIdSelected, tabIdPrevious) {
-  const tabNameSelected = $("#" + tabIdSelected + "-name");
-  const tabIconSelected = $("#" + tabIdSelected + "-icon");
-  const content = $("#content");
-
   if (tabIdPrevious) {
-    const tabNamePrevious = $("#" + tabIdPrevious + "-name");
-    const tabIconPrevious = $("#" + tabIdPrevious + "-icon");
-    tabNamePrevious.classList.remove("tab-name-selected");
-    tabIconPrevious.classList.remove("tab-icon-selected");
+    $("#" + tabIdPrevious + "-name").removeClass("tab-name-selected");
+    $("#" + tabIdPrevious + "-icon").removeClass("tab-icon-selected");
   }
 
-  tabNameSelected.classList.add("tab-name-selected");
-  tabIconSelected.classList.add("tab-icon-selected");
+  $("#" + tabIdSelected + "-name").addClass("tab-name-selected");
+  $("#" + tabIdSelected + "-icon").addClass("tab-icon-selected");
 
+  const content = $("#content");
   switch (tabIdSelected) {
     case "tab0":
-      content.innerHTML = `
-            <h1>Welcome to Tab 0</h1>
-            <p>This is the content of Tab 0.</p>
-            `;
+      $("#content").load("../html/dashboard.html");
       break;
     case "tab1":
-      content.innerHTML = `
+      content.html(`
             <h1>Welcome to Tab 1</h1>
             <p>This is the content of Tab 1.</p>
-          `;
+          `);
       break;
     case "tab2":
-      content.innerHTML = `
+      content.html(`
             <h1>Welcome to Tab 2</h1>
             <p>This is the content of Tab 2.</p>
-          `;
+          `);
       break;
     case "tab3":
-      content.innerHTML = `
+      content.html(`
             <h1>Welcome to Tab 3</h1>
             <p>This is the content of Tab 3.</p>
-          `;
+          `);
       break;
     case "tab4":
-      content.innerHTML = `
+      content.html(`
             <h1>Welcome to Tab 4</h1>
             <p>This is the content of Tab 4.</p>
-          `;
+          `);
       break;
     case "tab5":
-      content.innerHTML = `
+      content.html(`
             <h1>Welcome to Tab 5</h1>
             <p>This is the content of Tab 5.</p>
-          `;
+          `);
       break;
     default:
-      content.innerHTML = `
+      content.html(`
             <h1>Welcome to Empty Tab</h1>
             <p>This is the content of current Empty Tab.</p>
-          `;
+          `);
       break;
   }
 }
