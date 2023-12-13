d7bd9c110a1fefbf1bc7e43c3284ed0a0e9e234b
AxellLim00 <65800182+AxellLim00@users.noreply.github.com>
2023-12-14 00:26:49 +1030

    TO DO for Socket

--- src/utils/socket-utils.js
+++ src/utils/socket-utils.js
@@ -15,3 +15,5 @@ const instance = new Socket();
 Object.freeze(instance);
 
 export default instance.socket;
+
+// TODO: Make sure one socket is used once, may need global or session variable if all else fails
